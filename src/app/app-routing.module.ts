e9c5cb6ac26eb21c8eae9c3d96fa203696ad9f84
JaRodriguez97 <cristi542012@gmail.com>
2025-03-20 20:42:23 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./modules/Inicio/inicio.module').then((m) => m.InicioModule),
+  },
+  {
+    path: 'marco',
+    loadChildren: () =>
+      import('./modules/Marco/marco.module').then((m) => m.MarcoModule),
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes, {
+      initialNavigation: 'enabledBlocking',
+    }),
+  ],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/modules/Inicio/inicio.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-inicio',
+  templateUrl: './inicio.component.html',
+  styleUrls: ['./inicio.component.css'],
+})
+export class InicioComponent implements OnInit {
+  constructor(public route: Router) {}
+  ngOnInit(): void {
+    setTimeout(() => this.route.navigate(['marco']), 10000);
+  }
+}
