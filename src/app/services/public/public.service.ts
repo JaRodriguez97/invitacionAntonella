de4df51fdc09815244dc829c7642ecb3c44b35ad
JaRodriguez97 <cristi542012@gmail.com>
2025-03-21 09:50:59 -0500

    Update public.service.ts

--- src/app/services/public/public.service.ts
+++ src/app/services/public/public.service.ts
@@ -1,37 +1,46 @@
-import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { Injectable, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 @Injectable({
   providedIn: 'root',
 })
 export class PublicService {
-  private audio: HTMLAudioElement;
+  private audio!: HTMLAudioElement;
 
-  constructor(public route: Router) {
-    this.audio = new Audio();
-    this.audio.src = '../assets/audio.mpeg';
-    this.audio.loop = true; // Para que el audio no se detenga
-    this.audio.volume = 0.5;
+  constructor(
+    public route: Router,
+    @Inject(PLATFORM_ID) private platformId: object
+  ) {
+    if (isPlatformBrowser(this.platformId)) {
+      this.audio = new Audio();
+      this.audio.src = '../assets/audio.mpeg';
+      this.audio.loop = true; // Para que el audio no se detenga
+      this.audio.volume = 0.5;
+    }
   }
 
   playAudio() {
-    this.audio.play().catch((error) => {
-      if (
-        error.name === 'NotAllowedError' &&
-        error.code === 0 &&
-        error.message ===
-          "play() failed because the user didn't interact with the document first. https://goo.gl/xX8pDD"
-      )
-        this.route.navigate(['splash']);
-      else console.error('Error al reproducir el audio:', error);
-    });
+    if (isPlatformBrowser(this.platformId))
+      this.audio.play().catch((error) => {
+        if (
+          error.name === 'NotAllowedError' &&
+          error.code === 0 &&
+          error.message ===
+            "play() failed because the user didn't interact with the document first. https://goo.gl/xX8pDD"
+        )
+          this.route.navigate(['splash']);
+        else console.error('Error al reproducir el audio:', error);
+      });
   }
 
   pauseAudio() {
-    this.audio.pause();
+    if (isPlatformBrowser(this.platformId)) this.audio.pause();
   }
 
   isPlaying(): boolean {
-    return !this.audio.paused && this.audio.currentTime > 0;
+    if (isPlatformBrowser(this.platformId))
+      return !this.audio.paused && this.audio.currentTime > 0;
+
+    return false;
   }
 }
